2cb0c3fd5a02e10e916d34bd318e3ff8b86afd83
Diego Carrón Surribas <dcarron32@gmail.com>
2022-01-16 22:37:39 +0000

    Aarry asociativo

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Array indexado
+    $amigos=array("Antón","Rosalía","Xoán")
+    echo "O terceiro amgio é: " . $amgios[2]."<br/>";
+    //Aarry asociativo
+    $dnisAmigos=array("Antón"=>"12345678X","Rosalía"=>"11112222X","Xoan"=>"99997777X");
+    echo "O DNI de Rosalía é: " . $dnisAmigos['Rosalía']."<br/>";
+    ?>
+</body>
+</html>
