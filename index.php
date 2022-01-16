f13258024b08f2a152220c156c367b2b70d2d503
Diego Carrón Surribas <dcarron32@gmail.com>
2022-01-16 22:46:39 +0000

    Array de dúas dimensións

--- index.php
+++ index.php
@@ -10,10 +10,34 @@
     <?php
     // Array indexado
     $amigos=array("Antón","Rosalía","Xoán")
-    echo "O terceiro amgio é: " . $amgios[2]."<br/>";
+   echo "O segundo coche de Xoan é " . $cochesAmgios['Xoán'][1] ."<br/>";
     //Aarry asociativo
     $dnisAmigos=array("Antón"=>"12345678X","Rosalía"=>"11112222X","Xoan"=>"99997777X");
     echo "O DNI de Rosalía é: " . $dnisAmigos['Rosalía']."<br/>";
+    //Array de dúas dimensións:
+    $cochesAmgios = array
+    (
+    "Antón"=>array
+    )
+    (
+    "Fiat 500",
+    "Audi A3",
+    "Peugot 208"
+    ),
+    "Rosalía"=>array
+    (
+    "Audi TT"    
+    ),
+    "Xoan"=>array
+    (
+    "Jaguar XF",
+    "BMW 520d",
+    "Seat Ibiza"    
+    )
+    );
+
+    echo "O segundo coche de Xoan é " . $cochesAmgios['Xoán'][1] ."<br/>";
+
     ?>
 </body>
 </html>
